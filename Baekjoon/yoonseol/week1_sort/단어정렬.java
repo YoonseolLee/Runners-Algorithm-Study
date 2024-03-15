bd5e6dd41f97f202a4c76c77d10f65f311698094
YoonseolLee <thisyoon97@gmail.com>
2024-03-15 14:27:19 +0900

    Revert "[20240315] 이윤설 1주차 정렬 - 3문제 완료"
    This reverts commit 256d2f532d0406f72728ff30cd05ec4ba254ac7d.

--- Baekjoon/yoonseol/week1_sort/단어정렬.java
+++ Baekjoon/yoonseol/week1_sort/단어정렬.java
@@ -1,41 +1,4 @@
 package Baekjoon.yoonseol.week1_sort;
 
-import java.io.*;
-import java.util.*;
-
-class 단어정렬 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int repetition = Integer.parseInt(br.readLine());
-
-        String[] array = new String[repetition];
-        for (int i = 0; i < repetition; i++) {
-            String input = br.readLine();
-            array[i] = input;
-        }
-
-        String[] uniqueArr = sort(array);
-
-        for (String str : uniqueArr) {
-            System.out.println(str);
-        }
-    }
-
-
-    static String[] sort(String[] array) {
-        // 길이가 짧은 것 부터, 길이가 같으면 사전 순으로 정렬
-        Arrays.sort(array, (s1, s2) -> {
-            if (s1.length() == s2.length()) {
-                return s1.compareTo(s2);
-            } else {
-                return Integer.compare(s1.length(), s2.length());
-            }
-        });
-
-        // 중복 제거
-        LinkedHashSet<String> set = new LinkedHashSet<>(Arrays.asList(array));
-        String[] uniqueArr = set.toArray(new String[0]);
-
-        return uniqueArr;
-    }
+public class 단어정렬 {
 }
