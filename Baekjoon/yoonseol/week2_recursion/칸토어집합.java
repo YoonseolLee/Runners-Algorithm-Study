855ef94fe075de70cce4ace8bd747389507e3f0d
YoonseolLee <thisyoon97@gmail.com>
2024-03-22 21:34:13 +0900

    [20240322] 이윤설 2주차 재귀함수- 3문제 완료 (재커밋)

--- Baekjoon/yoonseol/week2_recursion/칸토어집합.java
+++ Baekjoon/yoonseol/week2_recursion/칸토어집합.java
@@ -1,21 +1,15 @@
 package Baekjoon.yoonseol.week2_recursion;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class 칸토어집합 {
-
-    /**
-     * 아이디어가 안 떠올라서 사실상 못 풀었음... ㅜㅜ
-     * 추후에 다시 풀 예정..
-     */
 
     static int n;
     static StringBuilder sb;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String str;
 
@@ -28,13 +22,12 @@ public class 칸토어집합 {
                 sb.append("-");
             }
 
-            func(0, len);   // 시작 인덱스, 시작 길이
-            System.out.println(sb);
+            func(0, len);
+            bw.write(String.valueOf(sb));
         }
     }
 
     public static void func(int start, int size) {
-        // 최소 단위
         if (size == 1) {
             return;
         }
